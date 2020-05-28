185c5429736890c94c870961c52c688b472d2b31
emenym <emenym@gmail.com>
2020-05-28 13:58:38 -0400

    cleanup

--- netdrive_index.py
+++ netdrive_index.py
@@ -1,18 +1,17 @@
 import os
 import subprocess
-import ctypes
 from easygui import *
 from timeit import Timer
 from storage import Settings
-
 from win32comext.shell import shell, shellcon
-import win32api
+
 
 FILEBROWSER_PATH = os.path.join(os.getenv('WINDIR'), 'explorer.exe')
 
+
 def main():
     msg = 'Select an action'
-    title = 'Derp'
+    title = 'File Finder'
     findAction = 'Find Files'
     settingsAction = 'Change a setting'
     exitAction = 'Exit'
@@ -21,6 +20,8 @@ def main():
 
     while 1:
         while not action:
+            if action is None:
+                exit(0)
             action = choicebox(msg, title, actionList)
 
         if action == findAction:
@@ -28,14 +29,12 @@ def main():
             action = False
         elif action == settingsAction:
             change_settings()
-            action = None
+            action = False
         elif action == exitAction:
             exit(0)
         else:
             exceptionbox('Unknown action', 'Error')
             exit(1)
-
-
 
 
 def do_find():
@@ -53,7 +52,7 @@ def do_find():
     settings.store()
 
     fieldNames = ["First Name", "Last Name"]
-    fieldValues = [settings.firstName, settings.lastName]  # we start with blanks for the values
+    fieldValues = [settings.firstName, settings.lastName]
     fieldValues = multenterbox(msg, title, fieldNames, fieldValues)
     while not fieldValues[0] or not fieldValues[1]:
         msgbox("Please provide both first and last names.")
@@ -67,14 +66,14 @@ def do_find():
         dumbway(res, term)
     else:
         path, files = find_files(res, fieldValues[0], fieldValues[1])
-        # t = Timer(lambda: find_files(res, term))
-        # time = t.timeit(number=1)
-        # print("time for find_files "+str(time))
+        if not files:
+            msgbox("No files found")
+            return
         open_resource(path, files)
+
 
 def find_files(res, term1, term2):
     derplist = []
-    # filesonly = (e for e in os.scandir(res) if e.is_file() and term1 in e.name.lower().replace("_", " "))
     if settings.caseSensitive == "False":
         filesonly = (e for e in os.scandir(res)
                      if e.is_file() and term1 == e.name.lower().split("_")[0] and term2 == e.name.lower().split("_")[1].split('.')[0])
@@ -85,8 +84,7 @@ def find_files(res, term1, term2):
     for f in filesonly:
         derplist.append(f.path)
     if not derplist:
-        msgbox("No files found")
-        exit(1)
+        return res, []
     derppath = os.path.dirname(derplist[0])
     for i in range(len(derplist)):
         derplist[i] = os.path.basename(derplist[i])
@@ -111,31 +109,17 @@ def change_settings():
 
 
 def evalStringTruth(str):
-    if str.lower() in ['true, True', 'yes', 'y', '1']:
+    if str.lower() in ['true', 'True', 't', 'yes', 'y', '1']:
         return 'True'
     else:
         return 'False'
 
-def derp2():
-    path = '\\\\KITCHEN\mike'
-    file1 = 'Meals_Oct_2012.xls'
-    folder_pidl = shell.SHILCreateFromPath(path, 0)[0]
-    desktop = shell.SHGetDesktopFolder()
-    shell_folder = desktop.BindToObject(folder_pidl, None, shell.IID_IShellFolder)
-    items = [item for item in shell_folder][:5]
-    shell.SHOpenFolderAndSelectItems(folder_pidl, items, 0)
-
-
-def remove_extension(path):
-    pass
 
 '''Given a absolute base path and names of its children (no path), open up one File Explorer window with all the child files selected'''
 def launch_file_explorer(path, files):
-    # path = '\\\\KITCHEN\mike'
     folder_pidl = shell.SHILCreateFromPath(path, 0)[0]
     desktop = shell.SHGetDesktopFolder()
     shell_folder = desktop.BindToObject(folder_pidl, None, shell.IID_IShellFolder)
-    # name_to_item_mapping = dict([(desktop.GetDisplayNameOf(item, 0), item) for item in shell_folder])
     name_to_item_mapping = dict([(desktop.GetDisplayNameOf(item, 1), item) for item in shell_folder])
     to_show = []
     for file in files:
@@ -145,56 +129,15 @@ def launch_file_explorer(path, files):
         shell.SHOpenFolderAndSelectItems(folder_pidl, to_show, 0)
 
 
-
-def creatfrompath(path, file):
-    path = 'D:\\Documents\\Python Projects\\netdrive_index\\test'
-    file = 'Big_Bopper_Tompson_1234455_adf.pfd'
-    #file1 = shellcon.SHGDN_FORPARSING
-    os.path.abspath(path+'\\'+file)
-
-
-    folder_pidl = shell.SHILCreateFromPath(path, 0)[0]
-    desktop = shell.SHGetDesktopFolder()
-    shell_folder = desktop.BindToObject(folder_pidl, None, shell.IID_IShellFolder)
-    # shell_folder.ParseDisplayName()<-------'https://docs.microsoft.com/en-us/windows/win32/api/shlobj_core/nf-shlobj_core-shparsedisplayname'
-    # file1 = shell.SHParseDisplayName(file, shellcon.SFGAO_BROWSABLE)
-    # file1 = shell_folder.SHParseDisplayName(file, shellcon.SFGAO_BROWSABLE)
-    # asdf = shell_folder.ParseDisplayName(desktop, file, shellcon.SFGAO_BROWSABLE)
-    asdf = shell.SHCreateItemFromParsingName(shell_folder, file, 0)
-
-    print('')
-
-
 def dumbway(res, term):
     filesonly = (e for e in os.scandir(res) if e.is_file() and term in e.name.lower().replace("_", " "))
     for f in filesonly:
         print(f.path)
-        # subprocess.Popen(r'explorer /select,'+f.path)
         subprocess.run([FILEBROWSER_PATH, '/select,', os.path.normpath(f.path)])
 
 
-def shat():
-    stramg = 'Tompson_mills_456_.pfd'
-    s1 = stramg.lower().replace('_',' ')
-
-
-    list1 = os.listdir(settings.targetServer)
-    for i in list1:
-        drep = i.split('_')
-        if settings.firstName == drep[0].split("_")[0] and settings.lastName == drep[1].split("_")[0].split('.')[0]:
-            print("asdf")
-
-
-
-
 if __name__ == "__main__":
-    # egdemo()
-    # settingsFilename = os.path.join("C:", "myApp", "settings.txt")  # Windows example
     settingsName = str(os.path.basename(__file__).split('.')[0]) + '.cfg'
     settingsFilename = os.path.join(os.getenv('LOCALAPPDATA'), settingsName)
-
     settings = Settings(settingsFilename)
-    shat()
-
-    # creatfrompath('','')
     main()
